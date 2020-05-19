9783ea3fe2678944e1f4698922488943fad3ac20
James Brotsos <j.brotsos@gmail.com>
2020-05-19 19:58:20 +0000

    fix

--- src/sample_xss.java
+++ src/sample_xss.java
@@ -33,7 +33,7 @@ public class BookDetail_jsp extends HttpJspBase {
 
 		String userAccount = request.getParameter("Account"); 
 
-		printSanitizedName(userAccount);
+		//printSanitizedName(userAccount);
 	}
 
 	public static void loadNumber () {
