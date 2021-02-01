01592c4cba5c95e9dc823b4657d0ff3e41c9cc8b
James Brotsos <j.brotsos@gmail.com>
2021-02-01 15:04:30 -0800

    Update sample_xss.java

--- src/sample_xss.java
+++ src/sample_xss.java
@@ -40,7 +40,7 @@ public class BookDetail_jsp extends HttpJspBase {
 
 		String userNumber = request.getParameter("Number"); 
 
-		printSanitizedName(userNumber);
+		//printSanitizedName(userNumber);
 	}
 
 	public static void loadField () {
